b7d1ed0bbb831664c8dab713783fdbde02187e59
Bryan <52042013+carbone84@users.noreply.github.com>
2020-11-20 09:45:35 -0500

    Create getOrgReport.py
    This is a sample script to get the organization report from pingid.
    
    It hits the 'rest/4/createjob/do' endpoint to create the job, the 'rest/4/getjobstatus/do' endpoint to check job status, and then the 'rest/4/getorgreport/do' to get the report. It then creates a csv file of the organization report.

---
+++ getOrgReport.py
@@ -0,0 +1,36 @@
+import pingid
+import os
+
+PROPERTIES_FILE = 'pingid.properties'
+org_report_file = "orgReport.csv"
+
+pingid = pingid.PingIDDriver(PROPERTIES_FILE, verbose=True)
+
+""" User Management API: Create Job """
+print("Calling Create Job...")
+create_job_body = {'jobType': "USER_REPORTS", 'clientData': "Session data echoed back to the requestor"}
+create_job_response = pingid.call('rest/4/createjob/do', create_job_body)
+job_token = create_job_response['responseBody']['jobToken']
+
+""" User Management API: Get Job Status"""
+job_status_body = {'jobToken': job_token, 'clientData': "Session data echoed back to the requestor"}
+job_status_response = 'PENDING'
+while job_status_response != 'DONE':
+    job_status_response = pingid.call('rest/4/getjobstatus/do', job_status_body)['responseBody']['jobResult']['status']
+    print(job_status_response)
+    if job_status_response == 'FAILURE':
+        print('Something went wrong...')
+        exit()
+
+""" User Mangement API: Get Organization Report """
+print("Calling Get Org Report...")
+org_report_body = {'fileType': "CSV", 'clientData': "Session data echoed back to the requestor"}
+org_report_response = pingid.call('rest/4/getorgreport/do', org_report_body)
+
+""" Writing report to CSV """
+print(f"Creating {org_report_file}...")
+org_report_csv = open(org_report_file, 'w', newline='')
+org_report_csv.write(org_report_response)
+org_report_csv.close()
+
+print(f"Org report successfully created. Located at: {org_report_file}")
